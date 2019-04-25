6c98a8692da5a4b26aefa6ece4e290a145f73779
Dani Hengeveld <dani10hengeveld@gmail.com>
2019-04-25 14:44:34 +0200

    upped

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/includes/links.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/includes/links.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="/css/fonts.css">
 <link href="https://fonts.googleapis.com/css?family=Arvo%7CEB+Garamond%7CLobster" rel="stylesheet">
+<link rel="stylesheet" href="/css/fonts.css">
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
 </script>
