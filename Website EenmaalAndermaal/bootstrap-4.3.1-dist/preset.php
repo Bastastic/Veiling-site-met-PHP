a95fd5f8b6beaece2fc3456466630dcb4ff44aa8
Dani Hengeveld <dani10hengeveld@gmail.com>
2019-04-25 13:44:08 +0200

    preset + shit

---
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/preset.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include '/includes/links.php'; ?>
+    <title>Document</title>
+</head>
+
+<?php include '/includes/header.php'; ?>
+<body>
+    
+</body>
+<?php include '/includes/footer.php'; ?>
+</html>
