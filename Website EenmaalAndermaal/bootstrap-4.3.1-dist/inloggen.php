9ec99e1c55e363c30503e4c559c3236dd5122b24
Wesley Smeltink <w.smeltink@student.han.nl>
2019-04-25 13:12:27 +0200

    Inlog + registreren (veranderd)

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/inloggen.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/inloggen.php
@@ -26,19 +26,16 @@
         <div class="imgcontainer">
         <img src="images/512px-Circle-icons-profile.svg.png" class="avatar" alt=""/>
         </div>
-        <label for="Gebruikersnaam"><b>Gebruikersnaam</b></label>
-        <input class="inlogbalk" type="text" placeholder="Gebruikernaam" name="Gebruikersnaam" required />
-        <label for="Wachtwoord"><b>Wachtwoord</b></label>
-        <input
-          class="inlogbalk"
-          type="password"
-          placeholder="Wachtwoord"
-          name="Wachtwoord"
-          required
-        />
-        <button class="inlogbutton" type="submit">Login</button>
-        <br>
-        <br>
+        <div class="col-xs-20 col-sm-20 col-md-20">
+        <div class="form-group">
+				<input type="text" name= "Gebruikersnaam" id="Gebruikersnaam" class="form-control input-lg" placeholder="Gebruikersnaam" tabindex="3">
+      </div>
+</div>
+      <div class="form-group">
+				<input type="text" name= "Wachtwoord" id="Wachtwoord" class="form-control input-lg" placeholder="Wachtwoord" tabindex="3">
+      </div>
+      <div class="col-xs-20 col-md-20"><input type="submit" value="Inloggen" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
+      <br>
           <input type="checkbox" checked="checked" name="onthoudme" /> onthouden 
           <span class="password">Forgot <a href="#">password?</a></span>
       </div>
