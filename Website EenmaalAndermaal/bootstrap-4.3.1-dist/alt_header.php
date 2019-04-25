79d9acfe700d5cb10bbcecc93213cc7092a5a73f
Peiwand <peiwand@hotmail.nl>
2019-04-25 12:22:19 +0200

    css gefixt

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/alt_header.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/alt_header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="custom/css/custom.css">
+    <link rel="stylesheet" href="css/fonts.css">
     <link href="https://fonts.googleapis.com/css?family=Arvo|EB+Garamond|Lobster" rel="stylesheet">
     <script src="js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
