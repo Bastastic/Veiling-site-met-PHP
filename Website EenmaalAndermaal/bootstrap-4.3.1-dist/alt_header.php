5965a9e6430868b2696f8e75cbef2700d6ac4f06
Dani Hengeveld <dani10hengeveld@gmail.com>
2019-04-25 10:34:55 +0200

    beautify

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/alt_header.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/alt_header.php
@@ -7,14 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="custom/css/custom.css">
-	<link href="https://fonts.googleapis.com/css?family=Arvo|EB+Garamond|Lobster" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arvo|EB+Garamond|Lobster" rel="stylesheet">
     <script src="js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
     <title>Document</title>
 </head>
 
