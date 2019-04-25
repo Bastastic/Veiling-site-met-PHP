69149c227597a9b821cef21f2ab4164761ac9180
SenadK95 <48954479+SenadK95@users.noreply.github.com>
2019-04-25 14:12:13 +0100

    slider toegevoegd aan biedingspagina

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/biedingspagina.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/biedingspagina.php
@@ -1,7 +1,60 @@
-
+<!DOCTYPE html>
+<html lang="nl">
 <head>
-<?php  include ('includes/links.php'); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include 'includes/links.php'; ?>
+    <title>Biedingspagina</title>
 </head>
 
-<?php  include ('includes/header.php'); ?>
+<style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+      width: 100%;
+      height: 100%;
+  }
 
+  .carousel {
+  width:60%;
+  height:auto;
+}
+  </style>
+
+<?php include 'includes/header.php'; ?>
+<body>
+    
+<div id="demo" class="carousel slide" data-ride="carousel" >
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.w3schools.com/bootstrap4/ny.jpg" alt="Los Angeles" >
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.w3schools.com/bootstrap4/ny.jpg" alt="Chicago" >
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.w3schools.com/bootstrap4/ny.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+</body>
+<?php include 'includes/footer.php'; ?>
+</html>
