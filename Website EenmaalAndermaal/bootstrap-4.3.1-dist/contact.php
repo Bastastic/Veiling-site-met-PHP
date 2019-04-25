c403c90094329d39170ea1e0c9d0a6986fac14d5
Sebastiaan Slijkhuis <bas.slijkhuis@student.han.nl>
2019-04-25 12:23:22 +0200

    Create contact.php

---
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/contact.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+ <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+ <script scr="js/bootstrap.min.js"></script>
+ <link href="css/login.css" rel="stylesheet">
+</head>
+<body>
+  </head>
+  <body>
+    <?php include('alt_header.php'); ?>
+        <br>
+  <div class="">
+ <div class="algemeencontainer">
+    <embed src="pdf/algemenevoorwaarden.pdf" width="100%" height="100%" alt="pdf" pluginspage="http://www.adobe.com/products/acrobat/readstep2.html">
+  </div>
+</div>
+</body>
+<footer>
+        <?php include('footer.php'); ?>
+</footer>
+</html>
+  
