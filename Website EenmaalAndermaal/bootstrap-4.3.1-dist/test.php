20d7c2f493024ddab43133f56aee935ee3ec9327
SenadK95 <48954479+SenadK95@users.noreply.github.com>
2019-05-07 10:25:40 +0200

    Create test.php

---
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include 'includes/links.php'; ?>
+    <title>EenmaalAndermaal</title>
+</head>
+
+<?php include 'includes/header.php'; ?>
+
+
+
+<body>
+
+
+
+
+
+
+</body>
