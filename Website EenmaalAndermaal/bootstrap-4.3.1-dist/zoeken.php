6f6ccaa6c9a5da5766b905ae8c10924a368ff8f7
Peiwand <peiwand@hotmail.nl>
2019-05-09 13:05:27 +0200

    opmaak document

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/zoeken.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/zoeken.php
@@ -10,25 +10,15 @@
     <title>Over ons</title>
 </head>
 <?php
-     require 'php/connectDB.php'; 
-    ?>
-
-
-<?php
-     include 'includes/header.php'; 
-    ?>
+    require 'php/connectDB.php'; 
+    include 'includes/header.php'; 
+?>
 
 <body style="overflow-x:hidden">
     <section id="team" class="pb-5">
-
-
-
         <div class="container">
             <br>
             <h2>Zoekresultaten</h2>
-
-
-
             <div class="row">
                 <?php
                 if(isset($_GET['cat']) && isset($_GET['zoeken'])){
@@ -44,65 +34,66 @@
                     $result = $sql->fetchAll(PDO::FETCH_ASSOC);
                     $aantal = $sql->rowCount();
                     if($aantal < 1){
-                     echo '<p class="ml-4">Geen producten gevonden</p>';   
+                        echo '<p class="ml-4">Geen producten gevonden</p>';   
                     }
-                    }
-                    elseif(isset($_GET['cat'])){
-                        $cat = $_GET['cat'];
+                } 
+                elseif(isset($_GET['cat'])) {
+                    $cat = $_GET['cat'];
 
-                        $sql = $dbh->prepare(
-                            "SELECT voorwerp, voorwerpnummer, titel, beschrijving, startprijs, looptijdeindedag, looptijdeindetijdstip 
-                            FROM Voorwerp, Voorwerp_in_Rubriek 
-                            WHERE Voorwerp.voorwerpnummer=Voorwerp_in_Rubriek.Voorwerp 
-                            AND Voorwerp_in_Rubriek.Rubriek_op_Laagste_Niveau =:cat");  
-                        $sql->execute(['cat' => $cat]);
-                        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-                    }
-                    elseif (isset($_GET['zoeken'])){
-                        $trefwoord = strval($_GET['zoeken']);
-                        $sql = $dbh->prepare(
-                            "SELECT voorwerpnummer, titel, beschrijving, startprijs, looptijdeindedag, looptijdeindetijdstip 
-                            FROM Voorwerp 
-                            WHERE titel LIKE CONCAT('%', :trefwoord, '%')");
-                        $sql->execute(['trefwoord' => $trefwoord]);
-                        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-                    }else{
-                        $sql = $dbh->prepare(
-                            "SELECT voorwerpnummer, titel, beschrijving, startprijs, looptijdeindedag, looptijdeindetijdstip 
-                            FROM Voorwerp");
-                        $sql->execute();
-                        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
-                    }
-                    foreach ($result as $key => $value) {
-                        $voorwerpnummer = $value['voorwerpnummer'];
-                        $titel = $value['titel'];
-                        $bescrhijving = $value['beschrijving'];
-                        $startprijs = $value['startprijs'];
-                        $looptijdeindedag = $value['looptijdeindedag'];
-                        $dag = date_create($looptijdeindedag);
-                        $dag = date_format($dag, "d-m-Y");
-                        $looptijdeindetijdstip = $value['looptijdeindetijdstip'];
+                    $sql = $dbh->prepare(
+                        "SELECT voorwerp, voorwerpnummer, titel, beschrijving, startprijs, looptijdeindedag, looptijdeindetijdstip 
+                        FROM Voorwerp, Voorwerp_in_Rubriek 
+                        WHERE Voorwerp.voorwerpnummer=Voorwerp_in_Rubriek.Voorwerp 
+                        AND Voorwerp_in_Rubriek.Rubriek_op_Laagste_Niveau =:cat");  
+                    $sql->execute(['cat' => $cat]);
+                    $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+                }
+                elseif (isset($_GET['zoeken'])) {
+                    $trefwoord = strval($_GET['zoeken']);
+                    $sql = $dbh->prepare(
+                        "SELECT voorwerpnummer, titel, beschrijving, startprijs, looptijdeindedag, looptijdeindetijdstip 
+                        FROM Voorwerp 
+                        WHERE titel LIKE CONCAT('%', :trefwoord, '%')");
+                    $sql->execute(['trefwoord' => $trefwoord]);
+                    $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+                }
+                else {
+                    $sql = $dbh->prepare(
+                        "SELECT voorwerpnummer, titel, beschrijving, startprijs, looptijdeindedag, looptijdeindetijdstip 
+                        FROM Voorwerp");
+                    $sql->execute();
+                    $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+                }
+                foreach ($result as $key => $value) {
+                    $voorwerpnummer = $value['voorwerpnummer'];
+                    $titel = $value['titel'];
+                    $bescrhijving = $value['beschrijving'];
+                    $startprijs = $value['startprijs'];
+                    $looptijdeindedag = $value['looptijdeindedag'];
+                    $dag = date_create($looptijdeindedag);
+                    $dag = date_format($dag, "d-m-Y");
+                    $looptijdeindetijdstip = $value['looptijdeindetijdstip'];
 
-                        echo "<div class='col-xs-12 col-sm-12 col-md-6' style='padding-top: 20px; cursor: pointer'
-                        onclick=\"window.location='biedingspagina.php?voorwerpnummer=" . $voorwerpnummer . "';\">
-                        <div class='image-flip' ontouchstart='this.classList.toggle('hover');'>
-                            <div class='mainflip'>
-                                <div class='frontside'>
-                                    <div class='card'>
-                                        <div class='card-body text-center'>
-                                            <p><img class=' img-fluid' src='images/test.jpg' alt='advertentie afbeelding'>
-                                            </p>
-                                            <h4>$titel</h4>
-                                            <p>$bescrhijving</p>
-                                            <h5>€$startprijs</h5>
-                                            <h6>Loopt af op $dag</h6>
-                                        </div>
+                    echo "<div class='col-xs-12 col-sm-12 col-md-6' style='padding-top: 20px; cursor: pointer'
+                    onclick=\"window.location='biedingspagina.php?voorwerpnummer=" . $voorwerpnummer . "';\">
+                    <div class='image-flip' ontouchstart='this.classList.toggle('hover');'>
+                        <div class='mainflip'>
+                            <div class='frontside'>
+                                <div class='card'>
+                                    <div class='card-body text-center'>
+                                        <p><img class=' img-fluid' src='images/test.jpg' alt='advertentie afbeelding'>
+                                        </p>
+                                        <h4>$titel</h4>
+                                        <p>$bescrhijving</p>
+                                        <h5>€$startprijs</h5>
+                                        <h6>Loopt af op $dag</h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>";
-                    }
+                    </div>
+                </div>";
+                }
                 ?>
             </div>
         </div>
