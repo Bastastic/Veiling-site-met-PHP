23ad71249731d766c4e4a56122ab0753fe528b63
Sebastiaan Slijkhuis <bas.slijkhuis@student.han.nl>
2019-05-15 12:39:51 +0200

    tester de test

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/php/connectDB.php
+++
@@ -1,8 +0,0 @@
-<?php
-setlocale(LC_ALL, 'NL_nl');
-$serverName = "mssql.iproject.icasites.nl";
-$uid = "iproject15";
-$pwd = "FkKqcryvuU";
-$databaseName = "iproject15";
-$dbh = new PDO("sqlsrv:Server=$serverName;Database=$uid", "$databaseName", "$pwd");
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
