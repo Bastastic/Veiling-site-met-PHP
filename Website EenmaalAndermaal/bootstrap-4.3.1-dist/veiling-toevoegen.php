f3bd40f6896e36965863b41d03499174c6143e51
Erkan Alper <erkan_alper66@hotmail.com>
2019-05-15 15:50:35 +0200

    Create veiling-toevoegen.php

---
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/veiling-toevoegen.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php include 'includes/links.php'; ?>
+    <title>Veiling toevoegen</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<style>
+    .admin-line {
+        border-top: 5px solid #ff814f;
+    }
+</style>
+
+
+<body style="overflow-x:hidden">
+    <?php include 'includes/header.php'; ?>
+    <div class="jumbotron">
+    </div> <!-- /container -->
+    <section id="team">
+        <div class="container">
+            <h5 class="section-title h1">Gekozen rubriek</h5>
+            <div class="row-full">
+                <hr class="admin-line">
+                <div class="row">
+
+
+                </div>
+            </div>
+            <!-- Images -->
+            <h5 class="section-title h1">Foto's</h5>
+            <div class="row-full">
+                <hr class="admin-line">
+                <div class="row">
+                </div>
+            </div>
+            <!-- Titel en beschrijving -->
+            <h5 class="section-title h1">Titel en beschrijving</h5>
+            <div class="row-full">
+                <hr class="admin-line">
+                <div class="row">
+                </div>
+            </div>
+
+        </div>
+</body>
+
+</html>
