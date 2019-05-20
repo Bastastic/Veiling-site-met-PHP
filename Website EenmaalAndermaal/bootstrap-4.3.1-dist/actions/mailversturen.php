4ecd17ca1a21241d1558018e7e6bc6af2cb1bfe9
SenadK95 <48954479+SenadK95@users.noreply.github.com>
2019-05-20 13:08:28 +0200

    Update mailversturen.php

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/actions/mailversturen.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/actions/mailversturen.php
@@ -1,27 +1,27 @@
 <?php
 require '../php/connectDB.php';
 
+
 $digits = 5;
 $controlegetal = mt_rand(pow(10, $digits-1), pow(10, $digits)-1);
-
-
+$gebruikersnaam= "peter1";
 
 if (isset($_POST['geklikt'])) {
     $mailadres = $_POST['emailadres'];
     $subject = "Verificatiecode voor uw account";
     $txt = "Hello Gebruiker! 
             Bedankt voor het registreren bij EenmaalAndermaal
-            Hierbij ontvangt u uw controlegetal:$controlegetal";
+            Hierbij ontvangt u uw controlegetal: $controlegetal";
     $headers = "From: contact@eenmaalandermaal.nl" . "\r\n";
 
     mail($mailadres, $subject, $txt, $headers);
 
      $querycode = "INSERT INTO Verificatie (Gebruikersnaam, Verificatiecode)
-     VALUES ( :gebruikersnaam, :controlegetal)";
+                   VALUES ( :gebruikersnaam, 
+                            :controlegetal)";
+
      $sqlcode = $dbh->prepare($querycode);
-     $sqlcode->bindValue(":gebruikersnaam", $gebruikersnaam);
-     $sqlcode->bindValue(":controlegetal", $controlegetal);
-     $sqlcode->execute();
+     $sqlcode->execute(['gebruikersnaam' => $gebruikersnaam, 'controlegetal' => $controlegetal ]);
 
     header("Location: ../mailverstuurd.php");
 }
