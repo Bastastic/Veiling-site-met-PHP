23b714052fb2b8b7af024b34bea3121f9ce415ef
Dani Hengeveld <dani10hengeveld@gmail.com>
2019-05-20 15:04:10 +0200

    Update mailversturen.php

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/actions/mailversturen.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/actions/mailversturen.php
@@ -1,60 +1,55 @@
 
 
 <?php
-
+session_start();
 require '../php/connectDB.php';
 
+$gebruikersnaam = $_SESSION['userID'];
 $emailadres = $_POST['emailadres'];
 
 
-$querygebruikersnaam = "SELECT Gebruikersnaam, Mailbox FROM Gebruiker WHERE Mailbox = :emailadres";
-$sqlgebruikersnaam = $dbh->prepare($querygebruikersnaam);
-$sqlgebruikersnaam->execute(['emailadres' => $emailadres]);
-$gebruikersnaam = $sqlgebruikersnaam->fetch(PDO::FETCH_ASSOC);
-$gebruikersnaam = $gebruikersnaam['Gebruikersnaam'];
+$query = "SELECT Mailbox FROM Gebruiker WHERE Gebruikersnaam = :gebruikersnaam";
+$sql = $dbh->prepare($querygebruikersnaam);
+$sql->execute(['gebruikersnaam' => $gebruikersnaam]);
+$mailbox = $mailbox->fetch(PDO::FETCH_ASSOC);
+$mailbox = $mailbox['Mailbox'];
 
-$controleadres = $mail['Mailbox'];
+if (isset($_POST['geklikt']) &&  $emailadres == $mailbox) {
+    $digits = 5;
+    $controlegetal = mt_rand(pow(10, $digits-1), pow(10, $digits)-1);
+    // $gebruikersnaam= "peter2";
 
-// $querymail = "SELECT Mailbox FROM Gebruiker WHERE Gebruikersnaam = :gebruikersnaam";
-// $sqlmail = $dbh->prepare($querymail);
-// $sqlmail->execute(['gebruikersnaam' => $gebruikersnaam]);
-// $mail = $sqlmail->fetch(PDO::FETCH_ASSOC);
- $controleadres = $mail['Mailbox'];
+    if (isset($_POST['geklikt'])) {
+        $mailadres = $emailadres;
+        $subject = "Verificatiecode voor uw account";
+        $txt = "
+        <html>
+        <head>
+        <title>Verificatiecode</title>
+        </head>
+        <body style='text-algin: center;'>
+        <h1>Uw Verificatiecode</h1>
+        <p>Voer deze code in na het inloggen op EenmaalAndermaal</p>
+        <h3>$controlegetal</h3>
+        </body>
+        </html>
+        ";
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From: noreply@eenmaalandermaal.nl" . "\r\n";
 
+        mail($mailadres, $subject, $txt, $headers);
 
-// $controleadres = 'peter2@gmail.com';
-
-if(isset($_POST['geklikt']) &&  $emailadres == $controleadres ){ 
-
-
-
-
-$digits = 5;
-$controlegetal = mt_rand(pow(10, $digits-1), pow(10, $digits)-1);
-// $gebruikersnaam= "peter2";
-
-if (isset($_POST['geklikt'])) {
-    $mailadres = $_POST['emailadres'];
-    $subject = "Verificatiecode voor uw account";
-    $txt = "Hello Gebruiker! 
-            Bedankt voor het registreren bij EenmaalAndermaal
-            Hierbij ontvangt u uw controlegetal: $controlegetal";
-    $headers = "From: contact@eenmaalandermaal.nl" . "\r\n";
-
-    mail($mailadres, $subject, $txt, $headers);
-
-     $querycode = "INSERT INTO Verificatie (Gebruikersnaam, Verificatiecode)
+        $querycode = "INSERT INTO Verificatie (Gebruikersnaam, Verificatiecode)
                    VALUES ( :gebruikersnaam, 
                             :controlegetal)";
 
-     $sqlcode = $dbh->prepare($querycode);
-     $sqlcode->execute(['gebruikersnaam' => $gebruikersnaam, 'controlegetal' => $controlegetal ]);
+        $sqlcode = $dbh->prepare($querycode);
+        $sqlcode->execute(['gebruikersnaam' => $gebruikersnaam, 'controlegetal' => $controlegetal ]);
 
-    header("Location: ../mailverstuurd.php");
-}
-
-
-}else{
+        header("Location: ../mailverstuurd.php");
+    }
+} else {
     header("Location: ../mailversturen.php");
 }
 
