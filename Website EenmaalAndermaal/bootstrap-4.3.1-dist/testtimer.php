390c947c56e77e6bef875c6fcd2279416790bc59
SenadK95 <48954479+SenadK95@users.noreply.github.com>
2019-05-21 11:09:10 +0200

    Update testtimer.php

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/testtimer.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/testtimer.php
@@ -9,13 +9,43 @@
 </head>
 
 <?php include 'includes/header.php'; ?>
-<body>
 
-<?php    
- 
+<body> 
 
 
+<script> 
+
+function timer(){ 
+var deadline = new Date("Jan 5, 2020 15:37:25").getTime(); 
+var x = setInterval(function() { 
+var now = new Date().getTime(); 
+var t = deadline - now; 
+var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+document.getElementById("demo").innerHTML = days + "d "  
++ hours + "h " + minutes + "m " + seconds + "s "; 
+    if (t < 0) { 
+        clearInterval(x); 
+        document.getElementById("demo").innerHTML = "Veiling afgelopen!"; 
+    } 
+}, 1000); 
+
+}
+
+</script> 
+
+<?php
+// timer("Jan 5, 2020 15:37:25");
+// timer();
+
+function myFunction() {
+    alert("Hello World!");
+  }
+  myFunction();
 ?>
+
 
 </body>
 
