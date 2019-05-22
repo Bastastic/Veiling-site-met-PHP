0fa08a0b21054f7e41174f42869d1cca6d565094
Peiwand <peiwand@hotmail.nl>
2019-05-22 10:58:59 +0200

    categorie tijdelijk disabled

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/includes/categorieen.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/includes/categorieen.php
@@ -2,20 +2,22 @@
     <!-- <div class="container">
     <div id="accordion"> -->
     <?php
-        function Rubrieken($parent = -1, $margin = '') {
-            global $dbh;
-            $q = $dbh->prepare("SELECT * FROM Rubriek WHERE Hoofdrubriek=$parent");
-            $q->execute();
-            $q_fetchAll = $q->fetchAll();
-            for($i = 0; $i < count($q_fetchAll); $i++){
-                echo '<label> '
-                    . $margin . '<input type="checkbox" class="filled-in" value="' . $q_fetchAll[$i]['Rubrieknummer'] . '">
-                    <span>' . $q_fetchAll[$i]['Rubrieknaam'] . '</span>
-                    </label><br>';
-                Rubrieken($q_fetchAll[$i]['Rubrieknummer'], $margin . '&nbsp; &nbsp; &nbsp; &nbsp;');
-            }
-        }
-        Rubrieken();
+        // function Rubrieken($parent = -1, $margin = '') {
+        //     global $dbh;
+        //     $q = $dbh->prepare("SELECT Rubrieknummer, Rubrieknaam FROM Rubriek WHERE Hoofdrubriek=$parent");
+        //     $q->execute();
+        //     $q_fetchAll = $q->fetchAll();
+        //     for($i = 0; $i < count($q_fetchAll); $i++){
+        //         echo '<label> '
+        //             . $margin . '<input type="checkbox" class="filled-in" value="' . $q_fetchAll[$i]['Rubrieknummer'] . '">
+        //             <span>' . $q_fetchAll[$i]['Rubrieknaam'] . '</span>
+        //             </label>';
+        //         Rubrieken($q_fetchAll[$i]['Rubrieknummer'], $margin . '&nbsp; &nbsp; &nbsp; &nbsp;');
+        //     }
+        // }
+        // Rubrieken();
+
+        
     ?>
     <!-- </div>
     </div> -->
