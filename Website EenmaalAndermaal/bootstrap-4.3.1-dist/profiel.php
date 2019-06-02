63ed73b745b2287becc0914f20374ef5fc2b762f
Peiwand <peiwand@hotmail.nl>
2019-06-02 17:03:25 +0200

    Update profiel.php

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/profiel.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/profiel.php
@@ -700,63 +700,6 @@
             </div>
         </div>
     </div>
-
-    <?php
-    // // hier wordt een check uitgevoerd op de ingevulde gegevens om te kijken of ze juist zijn
-    // include 'php/phpcreditcard.php';
-    // include 'php/IBANcheck.php';
-
-    // if ((isset($_POST['IBAN']) && $_POST['IBAN'] != "") && (isset($_POST['ccNummer']) && $_POST['ccNummer'] != "")) {
-    //     $banknaam = $_POST['bank'];
-    //     $IBAN = $_POST['IBAN'];
-    //     $ccNummer = $_POST['ccNummer'];
-
-    //     if (isValidIBAN($IBAN) == true && check_cc($ccNummer) == true) {
-    //         $sql = $dbh->prepare(
-    //             "INSERT INTO Verkoper VALUES (:gebruikersnaam, :banknaam, :IBAN, 'In afwachting', :ccNummer)"
-    //             );
-    //         $sql->execute(['gebruikersnaam' => $gebruikersnaam, 'banknaam' => $banknaam, 'IBAN' => $IBAN, 'ccNummer' => $ccNummer]);
-
-    //         $message = "Uw gegevens zijn opgestuurd, u krijgt binnenkort een mail hierover.";
-    //         echo "<script type='text/javascript'>alert('$message');</script>";
-    //     } else {
-    //         $message = "Uw gegevens worden niet herkend";
-    //         echo "<script type='text/javascript'>alert('$message');</script>";
-    //     }
-    // } elseif (isset($_POST['IBAN']) && $_POST['IBAN'] != "") {
-    //     $banknaam = $_POST['bank'];
-    //     $IBAN = $_POST['IBAN'];
-
-    //     if (isValidIBAN($IBAN)) {
-    //         $sql = $dbh->prepare(
-    //             "INSERT INTO Verkoper VALUES (:gebruikersnaam, :banknaam, :IBAN, 'In afwachting', null)"
-    //             );
-    //         $sql->execute(['gebruikersnaam' => $gebruikersnaam, 'banknaam' => $banknaam, 'IBAN' => $IBAN]);
-
-    //         $message = "Uw gegevens zijn opgestuurd, u krijgt binnenkort een mail hierover.";
-    //         echo "<script type='text/javascript'>alert('$message');</script>";
-    //     } else {
-    //         $message = "Deze IBAN wordt niet herkend";
-    //         echo "<script type='text/javascript'>alert('$message');</script>";
-    //     }
-    // } elseif (isset($_POST['ccNummer']) && $_POST['ccNummer'] != "") {
-    //     $ccNummer = $_POST['ccNummer'];
-
-    //     if (check_cc($ccNummer)) {
-    //         $sql = $dbh->prepare(
-    //             "INSERT INTO Verkoper VALUES (:gebruikersnaam, null, null, 'In afwachting', :ccNummer)"
-    //             );
-    //         $sql->execute(['gebruikersnaam' => $gebruikersnaam, 'ccNummer' => $ccNummer]);
-
-    //         $message = "Uw gegevens zijn opgestuurd, u krijgt binnenkort een mail hierover.";
-    //         echo "<script type='text/javascript'>alert('$message');</script>";
-    //     } else {
-    //         $message = "Dit creditcard nummer wordt niet herkend";
-    //         echo "<script type='text/javascript'>alert('$message');</script>";
-    //     }
-    // }
-?>
-
     <br>
 </body>
 <?php include 'includes/footer.php'; ?>
