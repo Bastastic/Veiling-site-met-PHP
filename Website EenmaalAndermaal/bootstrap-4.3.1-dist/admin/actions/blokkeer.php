97824aa779415e9cd0496b17c69c3fa40bf1dd75
Dani Hengeveld <dani10hengeveld@gmail.com>
2019-06-05 10:43:09 +0200

    pushen

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/admin/actions/blokkeer.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/admin/actions/blokkeer.php
@@ -2,15 +2,21 @@
 session_start();
 require '../../php/connectDB.php';
 if(isset($_SESSION['adminID'])){
-if( isset($_POST['submit'])){
+if(isset($_POST['gebruikersnaam']) && isset($_POST['reden'])){
     $gebruiker = $_POST['gebruikersnaam'];
     $reden = $_POST['reden'];
+    if($_POST['duur'] == ""){
+        $duur = null;
+    }else{
+        $duur = $_POST['duur'];
+    }
 
-        $gebruikersblokkeren = "INSERT INTO geblokkeerd (Gebruiker, Datum, Reden)
-            VALUES (:Gebruiker, GETDATE(), :Reden)";
+        $gebruikersblokkeren = "INSERT INTO geblokkeerd (Gebruiker, Datum, Reden, Duur)
+            VALUES (:Gebruiker, GETDATE(), :Reden, :Duur)";
             $blokkeer = $dbh->prepare($gebruikersblokkeren);
             $blokkeer->bindValue(":Gebruiker", $gebruiker);
-            $blokkeer->bindValue(":Reden", $reden);    
+            $blokkeer->bindValue(":Reden", $reden);
+            $blokkeer->bindValue(":Duur", $duur);    
             $blokkeer->execute();
             header('Location: ../verkoper-blokkeren.php');
     }else{
