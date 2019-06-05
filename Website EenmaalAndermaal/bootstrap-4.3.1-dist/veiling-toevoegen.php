9b98f3524aecde7551db2c3383340a75866819a4
Erkan Alper <44494027+Prareric@users.noreply.github.com>
2019-06-05 21:06:31 +0200

    veiling toevoegen revert

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/veiling-toevoegen.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/veiling-toevoegen.php
@@ -70,15 +70,8 @@
                                                 </div>
                                                 <div class="form-group col-xs-12 col-sm-12 col-md-4">
                                                     <h4 class="card-title">Looptijd (in dagen):</h4>
-                                                    <select class="custom-select" name="Looptijd">
-                                                        <option selected>Choose...</option>
-                
-                                                        <option value="1">1</option>
-                                                        <option value="3">3</option>
-                                                        <option value="5">5</option>
-                                                        <option value="7">7</option>
-                                                        <option value="10">10</option>
-                                                    </select>                                                </div>
+                                                    <input type="number" class="form-control"  name="Looptijd" min="1" max="60">
+                                                </div>
                                                 <div class="form-group col-xs-12 col-sm-12 col-md-4">
                                                     <h4 class="card-title">Verzendkosten (in euros):</h4>
                                                     <input type="number" class="form-control"  name="Verzondkosten">
@@ -104,8 +97,7 @@
                                                 <br>
                                                 <?php $cat = strip_tags($_POST['cat']);?> 
                                                 <input type="hidden" name="cat" value="<?=$cat;?>">
-                                                <input type="submit" value="Voorbeeld inzien en plaatsen" name="AdvertentieP" class="btn btn-primary btn-block btn-lg"
-                                                tabindex="7">
+                                                <input type="submit" value="Voorbeeld inzien en advertentie plaatsen" name="AdvertentieP" class="btn btn-primary btn-block btn-lg" tabindex="7">
                                                 </div>
                                             </div>
                                             </form>
