2486b6f916ec44a35f3bca361ca69aee84429b5a
Dani Hengeveld <dani10hengeveld@gmail.com>
2019-06-05 13:40:38 +0200

    Update login_action.php

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/actions/login_action.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/actions/login_action.php
@@ -13,41 +13,46 @@ $sql = $dbh->prepare("SELECT * FROM Admin WHERE Gebruikersnaam=:gebruikersnaam")
 $sql->execute(['gebruikersnaam' => $gebruikersnaam]);
 $admin = $sql->fetch(PDO::FETCH_ASSOC);
 
-// Iedere keer nadat er is ingelogd wordt er gecontroleerd of je bent geactiveerd. Als je bent geactiveerd kan je overal naar toe
-// anders wordt je steeds naar de mailversturen.php gestuurd.
+if (password_verify($wachtwoord, $admin['Wachtwoord'])) {
+    $_SESSION['adminID'] = $admin['Gebruikersnaam'];
+    header('Location: ../admin/index.php');
 
-$sql = $dbh->prepare("SELECT * FROM geblokkeerd WHERE Gebruiker=:gebruikersnaam");
-$sql->execute(['gebruikersnaam' => $gebruikersnaam]);
-$geblokkeerd = $sql->fetch(PDO::FETCH_ASSOC);
+} elseif (password_verify($wachtwoord, $gebruiker['Wachtwoord'])) {
+    
+    $sql = $dbh->prepare("SELECT * FROM geblokkeerd WHERE Gebruiker=:gebruikersnaam");
+    $sql->execute(['gebruikersnaam' => $gebruikersnaam]);
+    $geblokkeerd = $sql->fetch(PDO::FETCH_ASSOC);
+    
 
-$banOphefDatum = date("Y-m-d", strtotime(strtr($geblokkeerd['Datum'], '/', '-'). ' + ' . $geblokkeerd['Duur'] . ' days'));
-echo $banOphefDatum;
+    if ($geblokkeerd) {
 
-// if (password_verify($wachtwoord, $admin['Wachtwoord'])) {
-//     $_SESSION['adminID'] = $admin['Gebruikersnaam'];
-//     header('Location: ../admin/index.php');
-// } else if (password_verify($wachtwoord, $gebruiker['Wachtwoord'])) {
-//     if ($geblokkeerd) {
-//         if ($banOphefDatum <= date(Y-m-d)) {
-//             $sql = $dbh->prepare("DELETE * FROM geblokkeerd WHERE Gebruiker=:gebruiker");
-//             $sql->execute(['gebruiker' => $gebruikersnaam]);
-//             $_SESSION['userID'] = $gebruiker['Gebruikersnaam'];
-//             if ($gebruiker['Geactiveerd'] == 1) {
-//                 header('Location: ../profiel.php');
-//             } else {
-//                 header('Location: ../mailversturen.php');
-//             }
-//         } else {
-//             header('Location: ../geblokkeerd.php?gebruiker=' . $gebruikersnaam);
-//         }
-//     } else {
-//         $_SESSION['userID'] = $gebruiker['Gebruikersnaam'];
-//         if ($gebruiker['Geactiveerd'] == 1) {
-//             header('Location: ../profiel.php');
-//         } else {
-//             header('Location: ../mailversturen.php');
-//         }
-//     }
-// } else {
-//     header('Location: ../inloggen.php?errc=1');
-// }
+        if ($geblokkeerd['Duur'] == null) {
+            $banOphefDatum = $geblokkeerd['Datum'];
+        } else {
+            $banOphefDatum = date("Y-m-d", strtotime($geblokkeerd['Datum']. ' + ' . $geblokkeerd['Duur'] . ' days'));
+        }
+
+        if ($banOphefDatum <= date("Y-m-d")) {
+            $sql = $dbh->prepare("DELETE * FROM geblokkeerd WHERE Gebruiker=:gebruiker");
+            $sql->execute(['gebruiker' => $gebruikersnaam]);
+            $_SESSION['userID'] = $gebruikersnaam;
+            
+            if ($gebruiker['Geactiveerd'] == 1) {
+                header('Location: ../profiel.php');
+            } else {
+                header('Location: ../mailversturen.php');
+            }
+        } else {
+            header('Location: ../geblokkeerd.php?gebruiker=' . $gebruikersnaam);
+        }
+    } else {
+        $_SESSION['userID'] = $gebruiker['Gebruikersnaam'];
+        if ($gebruiker['Geactiveerd'] == 1) {
+            header('Location: ../profiel.php');
+        } else {
+            header('Location: ../mailversturen.php');
+        }
+    }
+} else {
+    header('Location: ../inloggen.php?errc=1');
+}
