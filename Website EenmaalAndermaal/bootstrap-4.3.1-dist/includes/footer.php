b22c155cd7c1866ca72adbd60d6ae4b47f1d2842
Peiwand <peiwand@hotmail.nl>
2019-06-06 11:30:46 +0200

    Update footer.php

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/includes/footer.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/includes/footer.php
@@ -1,45 +1,45 @@
 <link rel="stylesheet" href="css/footer.css">
 
 <footer class="page-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h5>EenmaalAndermaal</h5>
-                <div class="row">
-                    <div class="col-6">
-                        <ul class="list-unstyled">
-                            <li><a href="">Product</a></li>
-                            <li><a href="">Benefits</a></li>
-                            <li><a href="">Partners</a></li>
-                            <li><a href="overons.php">Team</a></li>
-                        </ul>
+        <div class="container footer">
+            <div class="row">
+                <div class="col">
+                    <h5>EenmaalAndermaal</h5>
+                    <div class="row">
+                        <!-- <div class="col-6">
+                            <ul class="list-unstyled">
+                                <li><a href="">Product</a></li>
+                                <li><a href="">Benefits</a></li>
+                                <li><a href="">Partners</a></li>
+                                <li><a href="overons.php">Team</a></li>
+                            </ul>
+                        </div> -->
+                        <div class="col-6">
+                            <ul class="list-unstyled">
+                                <li><a href="contact.php">Contact</a></li>
+                                <li><a href="algemenevoorwaarden.php">Algemene Voorwaarden</a></li>
+                                <li><a href="overons.php">Over Ons</a></li>
+                                <li><a href="faq.php">F.A.Q.</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="col-6">
-                        <ul class="list-unstyled">
-                            <li><a href="contact.php">Contact</a></li>
-                            <li><a href="algemenevoorwaarden.php">Algemene Voorwaarden</a></li>
-                            <li><a href="overons.php">Over Ons</a></li>
-                            <li><a href="faq.php">F.A.Q.</a></li>
-                        </ul>
-                    </div>
+                    <!-- <ul class="nav">
+                        <li class="nav-item col-3"><a href="" class="nav-link"><i class="fab fa-facebook fa-lg"></i></a></li>
+                        <li class="nav-item col-3"><a href="" class="nav-link"><i class="fab fa-twitter fa-lg"></i></a></li>
+                        <li class="nav-item col-3"><a href="" class="nav-link"><i class="fab fa-github fa-lg"></i></a></li>
+                        <li class="nav-item col-3"><a href="" class="nav-link"><i class="fab fa-instagram fa-lg"></i></a>
+                        </li>
+                    </ul> -->
+                    <br>
                 </div>
-                <ul class="nav">
-                    <li class="nav-item col-3"><a href="" class="nav-link"><i class="fab fa-facebook fa-lg"></i></a></li>
-                    <li class="nav-item col-3"><a href="" class="nav-link"><i class="fab fa-twitter fa-lg"></i></a></li>
-                    <li class="nav-item col-3"><a href="" class="nav-link"><i class="fab fa-github fa-lg"></i></a></li>
-                    <li class="nav-item col-3"><a href="" class="nav-link"><i class="fab fa-instagram fa-lg"></i></a>
-                    </li>
-                </ul>
-                <br>
-            </div>
-            <div class="col">
-                <p>EenmaalAndermaal is een bedrijf van iConcepts. Wij hebben de missie om een duurzaam en groen
-                    alternatief te bieden aan de monopoly die momenteel heerst op de Nederlandse veilingsmarkten. Onze
-                    doelen zijn groen in tegenstelling tot onze grootste concutent.</p>
-            </div>
-            <div class="col-md-10">
-                <p>&copy EenmaalAndermaal - iConcepts <?php echo date("Y"); ?></p>
+                <div class="col">
+                    <p>EenmaalAndermaal is een bedrijf van iConcepts. Wij hebben de missie om een duurzaam en groen
+                        alternatief te bieden aan de monopoly die momenteel heerst op de Nederlandse veilingsmarkten. Onze
+                        doelen zijn groen in tegenstelling tot onze grootste concutent.</p>
+                </div>
+                <div class="col-md-10">
+                    <p>&copy EenmaalAndermaal - iConcepts <?php echo date("Y"); ?></p>
+                </div>
             </div>
         </div>
-    </div>
 </footer>
