e772eeb1fc24dda535d1a039239a84e15a8c88f2
Erkan Alper <44494027+Prareric@users.noreply.github.com>
2019-06-10 22:30:37 +0200

    Ontwerpdocument bijgewerkt er missen nog een aantal user story's

--- Website EenmaalAndermaal/bootstrap-4.3.1-dist/php/connectDB.php
+++ Website EenmaalAndermaal/bootstrap-4.3.1-dist/php/connectDB.php
@@ -1,8 +1,8 @@
 <?php
 setlocale(LC_ALL, 'NL_nl');
-$serverName = "mssql.iproject.icasites.nl";
-$uid = "iproject15";
-$pwd = "FkKqcryvuU";
+$serverName = "LAPTOP-44J2VQK\MSSQLSERVER02";
+$uid = "sa";
+$pwd = "12345678";
 $databaseName = "iproject15";
 $dbh = new PDO("sqlsrv:Server=$serverName;Database=$databaseName", "$uid", "$pwd");
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
